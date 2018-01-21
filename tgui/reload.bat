ad12bb9a535c9a04ef0e173850b24e24b4f8627e
AnturK <AnturK@users.noreply.github.com>
2018-01-21 05:04:37 +0100

    Fixes tgui reload script

--- tgui/reload.bat
+++ tgui/reload.bat
@@ -5,5 +5,10 @@ for /f "tokens=3* delims= " %%a in (
 ) do (
     set documents=%%a %%b
 )
+<<<<<<< HEAD
 REM Copy assets to the BYOND cache
 cmd /c copy assets\* "%documents%\BYOND\cache" /y
+=======
+REM Copy to tmp subdirectories
+FOR /D %%G in ("%DocumentsFolder%\BYOND\cache\tmp*") DO (cmd /c copy assets\* %%G /y)
+>>>>>>> 982b225... Fixes tgui reload script (#34632)
